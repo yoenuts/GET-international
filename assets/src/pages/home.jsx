7ac3001a6fbc2ebd188e9c5ffefefdbcedd29560
Erlein Nicole <111441283+yoenuts@users.noreply.github.com>
2023-11-02 23:26:14 +0800

    adding assets to home

--- assets/src/pages/home.jsx
+++ assets/src/pages/home.jsx
@@ -5,9 +5,18 @@ class Home extends React.Component {
     render(){
         return(
             <div className="Home">
-                <h1>Welcome to Home!</h1>
-                <br></br>
-                <p>hehehehehehe</p>
+                <div className="home-top-div">
+                    <iframe 
+                        width="560" 
+                        height="315" 
+                        src="https://www.youtube.com/embed/4jJVMXrOb14?si=ASM9HO8AVzT-f7JA" 
+                        title="YouTube video player" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        allowfullscreen
+                    >
+                    </iframe>
+                </div>
             </div>
         );
     }
