7916a5c894c17fd002d49e6c4b7adb444fd55e23
Erlein Nicole <111441283+yoenuts@users.noreply.github.com>
2023-11-06 22:41:23 +0800

    modify color theme, add new component class(carousel)

---
+++ assets/src/components/Carousel.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+class Component extends React.Component {
+    render(){
+        return(
+            <div className="owl-carousel owl-theme">
+                <div className="item">
+                    <img src="imagehere.jpg"></img>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default carousel;
